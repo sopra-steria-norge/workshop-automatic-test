fd75c80a91bda7603e192ae4f43b241f7c7b4cc9
geirivarjerstad <geir.jerstad@soprasteria.com>
2022-04-18 06:56:30 +0000

    ..

--- testscript/test_running_process.sh
+++ testscript/test_running_process.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-status=$(curl -s --head -w %{http_code} https://uptime-app.azurewebsites.net -o /dev/null)
+status=$(curl -L -s --head -w %{http_code} https://uptime-app.azurewebsites.net -o /dev/null)
 echo $status
+
+if echo "$status" | grep -q 200
+then
+  # Send failed status to Uptime
+  echo "Sending OK"
+  status=$(curl -L -s --head -w %{http_code} https://uptime-app.azurewebsites.net/api/push/VpbyW98MKX\?msg=OK\&ping= -o /dev/null)
+fi
